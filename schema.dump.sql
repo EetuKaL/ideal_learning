28ae598c829dcf954f5b4868e855ff6db3cdd882
EetuKaL <eetukallioniemi96@gmail.com>
2023-12-22 12:23:18 +0200

    new schemas

--- schema.dump.sql
+++ schema.dump.sql
@@ -2,10 +2,10 @@
 -- PostgreSQL database dump
 --
 
--- Dumped from database version 13.13
--- Dumped by pg_dump version 13.13
+-- Dumped from database version 16.1
+-- Dumped by pg_dump version 16.1
 
--- Started on 2023-12-21 11:20:41
+-- Started on 2023-12-22 12:20:56
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
@@ -27,7 +27,7 @@ CREATE EXTENSION IF NOT EXISTS adminpack WITH SCHEMA pg_catalog;
 
 
 --
--- TOC entry 2992 (class 0 OID 0)
+-- TOC entry 4863 (class 0 OID 0)
 -- Dependencies: 2
 -- Name: EXTENSION adminpack; Type: COMMENT; Schema: -; Owner: 
 --
@@ -40,26 +40,26 @@ SET default_tablespace = '';
 SET default_table_access_method = heap;
 
 --
--- TOC entry 201 (class 1259 OID 16394)
--- Name: users; Type: TABLE; Schema: public; Owner: postgres
+-- TOC entry 221 (class 1259 OID 16422)
+-- Name: answer_options; Type: TABLE; Schema: public; Owner: postgres
 --
 
-CREATE TABLE public.users (
-    etunimi character varying,
-    sukunimi character varying,
-    id bigint NOT NULL,
-    kayttajatunnus character varying
+CREATE TABLE public.answer_options (
+    answer_text character varying NOT NULL,
+    answer_correct boolean NOT NULL,
+    answer_id bigint NOT NULL,
+    question_id bigint NOT NULL
 );
 
 
-ALTER TABLE public.users OWNER TO postgres;
+ALTER TABLE public.answer_options OWNER TO postgres;
 
 --
--- TOC entry 202 (class 1259 OID 16412)
--- Name: kayttajat_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+-- TOC entry 220 (class 1259 OID 16421)
+-- Name: answer_options_answer_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
 --
 
-CREATE SEQUENCE public.kayttajat_id_seq
+CREATE SEQUENCE public.answer_options_answer_id_seq
     START WITH 1
     INCREMENT BY 1
     NO MINVALUE
@@ -67,56 +67,157 @@ CREATE SEQUENCE public.kayttajat_id_seq
     CACHE 1;
 
 
-ALTER TABLE public.kayttajat_id_seq OWNER TO postgres;
+ALTER SEQUENCE public.answer_options_answer_id_seq OWNER TO postgres;
 
 --
--- TOC entry 2993 (class 0 OID 0)
--- Dependencies: 202
--- Name: kayttajat_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+-- TOC entry 4864 (class 0 OID 0)
+-- Dependencies: 220
+-- Name: answer_options_answer_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
 --
 
-ALTER SEQUENCE public.kayttajat_id_seq OWNED BY public.users.id;
-
-
---
--- TOC entry 2852 (class 2604 OID 16414)
--- Name: users id; Type: DEFAULT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY public.users ALTER COLUMN id SET DEFAULT nextval('public.kayttajat_id_seq'::regclass);
+ALTER SEQUENCE public.answer_options_answer_id_seq OWNED BY public.answer_options.answer_id;
 
 
 --
--- TOC entry 2985 (class 0 OID 16394)
--- Dependencies: 201
--- Data for Name: users; Type: TABLE DATA; Schema: public; Owner: postgres
+-- TOC entry 218 (class 1259 OID 16408)
+-- Name: exams; Type: TABLE; Schema: public; Owner: postgres
 --
 
-COPY public.users (etunimi, sukunimi, id, kayttajatunnus) FROM stdin;
-mikko	mallikas	2	\N
-keijo	keinokas	1	\N
-\.
+CREATE TABLE public.exams (
+    exam_name character varying NOT NULL,
+    exam_id bigint NOT NULL,
+    published_at date,
+    updated_at date,
+    created_at date NOT NULL
+);
+
+
+ALTER TABLE public.exams OWNER TO postgres;
+
+--
+-- TOC entry 217 (class 1259 OID 16407)
+-- Name: exams_exam_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.exams_exam_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER SEQUENCE public.exams_exam_id_seq OWNER TO postgres;
+
+--
+-- TOC entry 4865 (class 0 OID 0)
+-- Dependencies: 217
+-- Name: exams_exam_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public.exams_exam_id_seq OWNED BY public.exams.exam_id;
 
 
 --
--- TOC entry 2994 (class 0 OID 0)
--- Dependencies: 202
--- Name: kayttajat_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+-- TOC entry 219 (class 1259 OID 16414)
+-- Name: questions; Type: TABLE; Schema: public; Owner: postgres
 --
 
-SELECT pg_catalog.setval('public.kayttajat_id_seq', 2, true);
+CREATE TABLE public.questions (
+    question_text character varying NOT NULL,
+    exam_id bigint NOT NULL,
+    question_id bigint DEFAULT nextval('public.exams_exam_id_seq'::regclass) NOT NULL
+);
+
+
+ALTER TABLE public.questions OWNER TO postgres;
+
+--
+-- TOC entry 216 (class 1259 OID 16399)
+-- Name: users; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.users (
+    first_name character varying,
+    last_name character varying,
+    user_email character varying NOT NULL,
+    user_password character varying NOT NULL
+);
+
+
+ALTER TABLE public.users OWNER TO postgres;
+
+--
+-- TOC entry 4704 (class 2604 OID 16425)
+-- Name: answer_options answer_id; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.answer_options ALTER COLUMN answer_id SET DEFAULT nextval('public.answer_options_answer_id_seq'::regclass);
 
 
 --
--- TOC entry 2854 (class 2606 OID 16422)
--- Name: users kayttajat_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+-- TOC entry 4702 (class 2604 OID 16411)
+-- Name: exams exam_id; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.exams ALTER COLUMN exam_id SET DEFAULT nextval('public.exams_exam_id_seq'::regclass);
+
+
+--
+-- TOC entry 4712 (class 2606 OID 16429)
+-- Name: answer_options answer_options_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.answer_options
+    ADD CONSTRAINT answer_options_pkey PRIMARY KEY (answer_id);
+
+
+--
+-- TOC entry 4708 (class 2606 OID 16431)
+-- Name: exams exams_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.exams
+    ADD CONSTRAINT exams_pkey PRIMARY KEY (exam_id);
+
+
+--
+-- TOC entry 4710 (class 2606 OID 16433)
+-- Name: questions questions_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.questions
+    ADD CONSTRAINT questions_pkey PRIMARY KEY (question_id);
+
+
+--
+-- TOC entry 4706 (class 2606 OID 16456)
+-- Name: users users_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
 ALTER TABLE ONLY public.users
-    ADD CONSTRAINT kayttajat_pkey PRIMARY KEY (id);
+    ADD CONSTRAINT users_pkey PRIMARY KEY (user_email);
 
 
--- Completed on 2023-12-21 11:20:42
+--
+-- TOC entry 4713 (class 2606 OID 16445)
+-- Name: questions fk_exam; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.questions
+    ADD CONSTRAINT fk_exam FOREIGN KEY (exam_id) REFERENCES public.exams(exam_id) ON DELETE CASCADE NOT VALID;
+
+
+--
+-- TOC entry 4714 (class 2606 OID 16450)
+-- Name: answer_options fk_question; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.answer_options
+    ADD CONSTRAINT fk_question FOREIGN KEY (question_id) REFERENCES public.questions(question_id) ON DELETE CASCADE NOT VALID;
+
+
+-- Completed on 2023-12-22 12:20:56
 
 --
 -- PostgreSQL database dump complete
